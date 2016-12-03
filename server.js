c223730962b5416b960553caf391a1d322b28989
Ajayk7 <ajay_k7@ymail.com>
2016-12-03 20:09:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,9 +94,9 @@ app.get('/counter', function( req ,res ) {
    res.send(counter.toString());
 });
 var names = [];
-app.get('/submit-name/:name', function(req,res) {
+app.get('/submit-name', function(req,res) { // url : /submit-name?name=xxx
    //Get the name from request object 
-   var name=req.params.name;
+   var name=req.query.name;
    
    names.push(name);
    
