01086f774dbd9a6b29d605d9185bd67d753baed4
Ajayk7 <ajay_k7@ymail.com>
2016-12-03 19:57:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,3 @@
-var element = document.getElementById('imad');
-var marginLeft = 0;
-
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    element.style.marginLeft = marginLeft + 'px';
-}
-
-element.onclick =function(){
-    var interval = setInterval(moveRight ,50);
-};
-
 //counter
 var button = document.getElementById('counter');
 
@@ -34,3 +22,22 @@ button.onclick = function() {
     request.send(null);
     
 };
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //should make a request to the server and send the name 
+    
+    //capture a list of name and render as a list
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i=0;i<names.length;i++) {
+        list += '<li>' + names[i] + '</li>'; 
+        
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+}
