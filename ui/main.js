11896c3ebb1a43dfa844a170c0212bdf1c4f4af5
Ajayk7 <ajay_k7@ymail.com>
2016-12-03 19:21:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,3 @@
-console.log('Loaded!');
 var element = document.getElementById('imad');
 var marginLeft = 0;
 
@@ -10,3 +9,17 @@ function moveRight(){
 element.onclick =function(){
     var interval = setInterval(moveRight ,50);
 }
+
+//counter
+var counter =0;
+var button = document.getElementById('counter');
+button.onclick = function() {
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in the variable
+    
+    //Render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
