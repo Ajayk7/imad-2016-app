8436e5c3d8000350a0187d2baeff934597e4f0c2
Ajayk7 <ajay_k7@ymail.com>
2016-12-03 20:24:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onclick = function(){
               //capture a list of name and render as a list
             var names = request.responseText;
             names = JSON.parse(names);
-            var list = ' ';
+            var list = '';
             for(var i=0;i<names.length;i++) {
                 list += '<li>' + names[i] + '</li>'; 
             }
